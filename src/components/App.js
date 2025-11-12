3a0ac91d85d8e2da8cf3b58d4f46e7fd10cc2cca
swallehrashid <swallehmtawazo@gmail.com>
2025-11-13 01:51:49 +0300

    adds a fetch using useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, {useState, useEffect} from 'react'
+
+function App() {
+    const [imageLink, setImageLink] = useState("")
+    const [isLoaded, setIsLoaded]=useState(true)
+    
+  
+
+  useEffect(()=>{
+    fetch('https://dog.ceo/api/breeds/image/random')
+     .then((res)=>res.json())
+     .then((data)=> {
+        setImageLink(data.message)
+        setIsLoaded(false)
+     })
+  },[])
+
+    if(isLoaded) {
+    return <p>Loading...</p>
+    }
+  return (
+    <div>
+      <img src={imageLink} alt= "A Random Dog"/>
+    </div>
+  )
+}
+
+export default App
